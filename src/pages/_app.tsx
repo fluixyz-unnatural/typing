ee6896c8ee7329745973c95c69ef621b1f3a6ca4
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-03 23:31:47 +0900

    head整備

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,23 @@ import 'tailwindcss/tailwind.css'
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head></Head>
+      <Head>
+        <meta
+          name="description"
+          content="ミスしたときにBackspaceを使わなければいけないローマ字入力タイピングゲーム"
+        />
+        <meta property="og:title" content="ページのタイトル" />
+        <meta
+          property="og:description"
+          content="ミスしたときにBackspaceを使わなければいけないローマ字入力タイピングゲーム"
+        />
+        <meta property="og:image" content="画像のURL" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@yoigara3" />
+        <meta property="og:site_name" content="サイト名" />
+        <meta property="og:locale" content="ja_JP" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
