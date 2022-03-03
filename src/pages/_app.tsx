ac5be68428c827ddbbcc17801a73c36fb70bed56
fluixyz-unnatural <sudo3949@gmail.com>
2022-03-03 16:48:33 +0900

    小規模開発はtailwindしか勝たん

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,11 @@
 import Head from 'next/head'
 import '../styles/global.css'
+import 'tailwindcss/tailwind.css'
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-      </Head>
+      <Head></Head>
       <Component {...pageProps} />
     </>
   )
